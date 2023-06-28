dda82b6432f85fdde267f7b686f15b981b3f54c9
Kenta Yamada <kentayamada058@gmail.com>
2023-06-28 15:52:06 +0900

    update

--- CS/DDD/Domain/Shared.cs
+++ CS/DDD/Domain/Shared.cs
@@ -1,9 +1,15 @@
-﻿namespace Domain
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Domain
 {
   public static class Shared
   {
+    private static readonly IConfiguration _configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile(path: "appsettings.json").Build();
+
     public static string? UserName { get; set; } = null;
-    public static bool IsFake => false;
+    public static bool IsFake => _configuration["IsFake"] == "True";
     public static bool IsDebugMode =>
 #if DEBUG
       true;
