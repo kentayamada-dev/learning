abd0aaf6fff2aeeaa0207b4d3e420db09f3d74dd
Kenta Yamada <kentayamada058@gmail.com>
2023-07-03 10:49:52 +0900

    init wpf

---
+++ CS/DDD/Wpf/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Wpf.Views;
+
+namespace Wpf
+{
+  public partial class App : PrismApplication
+  {
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+
+    protected override Window CreateShell()
+    {
+      return Container.Resolve<MainWindow>();
+    }
+  }
+}
---
+++ CS/DDD/Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+
+namespace Wpf.ViewModels
+{
+  public class MainWindowViewModel : BindableBase
+  {
+    private string _title = "Main Window";
+    public MainWindowViewModel()
+    {
+    }
+
+    public string Title
+    {
+      get => _title;
+      set => SetProperty(ref _title, value);
+    }
+  }
+}
